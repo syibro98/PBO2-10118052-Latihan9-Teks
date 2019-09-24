052f079d0595a5784a8f9db93d897d680c7439fb
Bagus Syibro Malisi <55564197+syibro98@users.noreply.github.com>
2019-09-24 12:45:39 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118052/latihan9/teks/PBO210118052Latihan9Teks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118052.latihan9.teks;
+
+/**
+ * @author 
+ * NAMA      : Bagus Syibro Malisi
+ * KELAS     : PBO2
+ * NIM       : 10118052
+ * Deskripsi Program : Program ini berisi program untuk menmapilkan atau 
+ * menginput teks atau data
+ */
+public class PBO210118052Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
